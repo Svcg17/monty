aaae21caaab2d33c5e881331e425cbc7592967c2
Sofia Cheung <659@holbertonschool.com>
2019-05-15 21:59:15 +0000

    Fixed struct:

--- getCoco.c
+++ getCoco.c
@@ -5,23 +5,16 @@ int getCoco(char *str, stack_t **stack, unsigned int line_number)
 	size_t i;
 
 	instruction_t ops[] = {
-//		{"push", _pErro}
-		/*
-		{"pop", _pop},
-		{"push", _push},
-		{"pall", _pall},
 		{"pint", _pint},
-		{"swap", _swap}
-		{"add", _add},
-		{"nop", _nop},*/
+		{"pop", _pop},
 		{"pall", _pall},
+		{"swap", _swap},
 		{NULL, NULL}
 	};
 
 	for (i = 0; ops[i].opcode != NULL; i++)
 		if (strcmp(ops[i].opcode, str) == 0)
 		{
-			printf("Calling %s\n", ops[i].opcode);
 			ops[i].f(stack, line_number);
 			return (1);
 		}
