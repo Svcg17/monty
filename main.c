e2c5aca163751ee31949e5ff4d71a2eec64629e6
Sofia Cheung <659@holbertonschool.com>
2019-05-15 02:22:32 +0000

    Added if condition to check if the token matches struct op

--- main.c
+++ main.c
@@ -5,23 +5,26 @@ int main(int argc, char **argv)
 	size_t i;
 	unsigned int nLine = 0;
 	char str[100];
-	stack_t *head;
+	stack_t *head = NULL;
 	char *token;
+	void *funct();
 	FILE *fp;
 
 	if (argc < 1)
 		exit (101);
-	fp = fopen(argv[1], "r");
+	fp = fopen("bytecode/00.m", "r");
 	while (fgets(str, 100, fp))
 	{
 		nLine++;
 		token = strtok(str, " \t");
 		while (token != NULL)
 		{
-			(*get_line(token))(&head, nLine);
+			if (getCoco(token, &head, nLine) == 1)
+				printf("token: %s\n", token);
 			token = strtok(NULL, " \t");
 		}
 	}
 	printf("%u\n", nLine);
+	close(fp);
 	return (0);
 }
